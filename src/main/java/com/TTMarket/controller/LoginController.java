47e4a6888c9c650bc761779e384c6bb2efaffcbb
Young5097 <bigfaith0301@naver.com>
2024-06-18 09:19:22 +0900

    fix02. gitignore 변경

--- src/main/java/com/TTMarket/controller/LoginController.java
+++ src/main/java/com/TTMarket/controller/LoginController.java
@@ -11,7 +11,6 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class LoginController {
-	
 	private Logger logger = LoggerFactory.getLogger(getClass());
 	
     @GetMapping("/")
